a865dc5d8ec149d5a8920920d17f71c1c33681ee
RP-PC07 <163131199+rp-pc05@users.noreply.github.com>
2025-04-16 15:08:58 -0300

    Adicionado integração com PostgreSQL

--- gamefyme/gamefyme/settings.py
+++ gamefyme/gamefyme/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gamefyme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gamefymedb',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'gamefymedb.c1g806ikoooe.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
