9c2464e022b962deb06e89df336aa269054dacb5
RP-PC07 <163131199+rp-pc05@users.noreply.github.com>
2025-04-25 15:44:28 -0300

    Ajustes

--- gamefyme/atividades/views.py
+++ gamefyme/atividades/views.py
@@ -3,6 +3,7 @@ from .forms import AtividadeForm
 from django.utils import timezone
 from services import login_service, atividades_service
 from django.contrib import messages
+from django.db import IntegrityError
 
 def criar_atividade(request):
     if not login_service.is_usuario_logado(request):
@@ -13,20 +14,23 @@ def criar_atividade(request):
     if request.method == 'POST':
         form = AtividadeForm(request.POST)
         if form.is_valid():
-            atividade = form.save(commit=False)
-            atividade.idusuario = usuario
-            atividade.dtatividade = timezone.now().date()
-            atividade.situacao = 'iniciada'
+            try:
+                atividade = form.save(commit=False)
+                atividade.idusuario = usuario
+                atividade.dtatividade = timezone.now().date()
+                atividade.situacao = 'iniciada'
 
-            atividade.expatividade = atividades_service.calcular_experiencia(
-                atividade.peso,
-                atividade.tpestimado
-            )
+                atividade.expatividade = atividades_service.calcular_experiencia(
+                    atividade.peso,
+                    atividade.tpestimado
+                )
 
-            atividade.save()
+                atividade.save()
 
-            messages.success(request, f'Atividade "{atividade.nmatividade}" criada com sucesso!')
-            return redirect('usuarios:main')
+                messages.success(request, f'Atividade "{atividade.nmatividade}" criada com sucesso!')
+                return redirect('usuarios:main')
+            except IntegrityError:
+                messages.error(request, f'Já existe uma atividade chamada "{form.cleaned_data["nmatividade"]}". Por favor, escolha um nome diferente.')
     else:
         form = AtividadeForm()
 
