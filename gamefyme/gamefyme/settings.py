2a30cb2aaa793d965ea50f16df905926aab5cf92
lucasminozzo <lucasmavila@gmail.com>
2025-05-14 22:09:27 -0300

    Validação de senha implementado

--- gamefyme/gamefyme/settings.py
+++ gamefyme/gamefyme/settings.py
@@ -91,16 +91,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'usuarios.validators.RequisitosSenhaValidator',  # validator personalizado REGRA DE NEGOCIO 1
     },
 ]
 
