d87aba9aab90bfd2a8d0abe8afb1072f40ed23ff
RP-PC07 <163131199+rp-pc05@users.noreply.github.com>
2025-05-22 15:20:57 -0300

    deploy

--- gamefyme/gamefyme/settings.py
+++ gamefyme/gamefyme/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
